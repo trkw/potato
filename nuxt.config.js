fbc41a903009a7b99353afb35151711319aa3aed
James Towers <james@songdrop.com>
2017-12-04 10:23:52 +0000

    Nuxt.js init

---
+++ nuxt.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'potato',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'FE Dev Tech Test' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  css: [
+    { src: '~/assets/css/app.css' }
+  ],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+
+  plugins: [
+    { src: '~/plugins/global.js', ssr: true }
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
